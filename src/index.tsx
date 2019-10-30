c3e747e899b9f7ab31db3887eb1590e3fdd0b202
Marco Medrano <marco.medrano@talkdesk.com>
2019-10-30 10:31:56 -0400

    Working TS with electron

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// Made with love using this url https://www.codementor.io/randyfindley/how-to-build-an-electron-app-using-create-react-app-and-electron-builder-ss1k0sfer
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
